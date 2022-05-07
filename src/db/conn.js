cb3040112123f70f17ffebb1224652eee0ca9c4f
unknown <er.gautamarya@gmail.com>
2022-05-07 12:10:13 +0530

    script update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
+const mongoURI  = 'mongodb+srv://users:users@cluster0.vkr3a.mongodb.net/users?retryWrites=true&w=majority';
 
-mongoose.connect("mongodb://127.0.0.1:27017/cms",{useNewUrlParser:true}).then(()=>{
+mongoose.connect(mongoURI ,{
+    useNewUrlParser:true,
+    // useCreateIndex:true,
+    // useUnifiedTopology:true,
+    // useFindAndModify:false
+}).then(()=>{
     console.log(`Connection Successful With Mongo DB`);
 }).catch((error)=>{
     console.log(error);
